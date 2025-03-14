0b3f6436c9a5b953d560554eb5cc8d74248d3907
bababoi880 <aliacayer@gmail.com>
2025-03-14 16:26:18 -0400

    evaluation echiquier

--- AlgoChess/Assets/NegaMax.cs
+++ AlgoChess/Assets/NegaMax.cs
@@ -12,5 +12,24 @@ public class NewMonoBehaviourScript
     * roi = 10 000
     * pour piece noir -> mettre valeur en negatif
     * */
+/*
+ *  public int EvaluerEchiquier(Plateau plateau, Couleur joueurActuel)
+{
+    int score = 0;
+    //score materiel
+    foreach(Piece piece in plateau.pieces)//parcourir le tableau de piece
+    {
+        int valeur = GetValeurPieces(piece); 
+        // si couleur cest la meme que le joueur actuel alors on ajoute au score 
+
+        score += (piece.getCouleur == joueurActuel) ? valeur : -valeur; 
+
+    }
+    score += EvaluerControleCentre(Plateau plateau,joueurActuel);
+
+
+    return score;
+}
+*/
 
 }
